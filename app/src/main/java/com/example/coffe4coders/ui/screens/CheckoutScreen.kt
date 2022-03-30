8e78575ed4d7e3ede2a2b20b94fe6510508add43
Jose C Morgado Vargas <josecruzmv@outlook.com>
2022-03-30 11:15:33 -0600

    Added: Custom TextField

---
+++ app/src/main/java/com/example/coffe4coders/ui/screens/CheckoutScreen.kt
@@ -0,0 +1,28 @@
+package com.example.coffe4coders.ui.screens
+
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.coffe4coders.ui.components.CustomTextField
+import com.example.coffe4coders.ui.theme.Coffe4CodersTheme
+
+@Composable
+fun CheckoutScreen() {
+    var name by remember { mutableStateOf("") }
+    CustomTextField(
+        value = name,
+        placeholder = "Name",
+        onValueChange = {
+            name = it
+        }
+    )
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun CheckoutScreenPreview() {
+    Coffe4CodersTheme {
+        CheckoutScreen()
+    }
+}
